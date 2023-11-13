9e7f86e651853a234c6ba539d80601c6df72d161
Augusto Guchea <aguchealezama@gmail.com>
2023-11-13 12:00:41 -0300

    Algunos fix

--- ruitz/views.py
+++ ruitz/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import Producto
-from django.db.models import Q
 from .models import *
+from .funcionalidades import *
+
 
 def Home(request):
     talles = Tamaño.objects.all()
@@ -10,12 +11,7 @@ def Home(request):
     productos = Producto.objects.all()
     busqueda = request.GET.get('buscador')
     if busqueda:
-        productos_relacionados = productos.filter(
-            Q(nombre__icontains=busqueda) |
-            Q(categoria__nombre__icontains=busqueda) |
-            Q(colores__nombre__icontains=busqueda)).distinct()
-        productos_no_relacionados = productos.exclude(id__in=productos_relacionados.values_list('id', flat=True))
-        productos = list(productos_relacionados) + list(productos_no_relacionados)
+        productos = buscar(busqueda)
         return render(request, "indumentaria.html", {'productos': productos, 'talles': talles, 'colores': colores, 'categorias': categorias})
     return render(request, "home.html", {'productos': productos, 'talles': talles, 'colores': colores, 'categorias': categorias})
 
@@ -27,11 +23,5 @@ def Indumentaria(request):
 
     busqueda = request.GET.get('buscador')
     if busqueda:
-        productos_relacionados = productos.filter(
-            Q(nombre__icontains=busqueda) |
-            Q(categoria__nombre__icontains=busqueda) |
-            Q(colores__nombre__icontains=busqueda)).distinct()
-        productos_no_relacionados = productos.exclude(id__in=productos_relacionados.values_list('id', flat=True))
-        productos = list(productos_relacionados) + list(productos_no_relacionados)
-
+        productos = buscar(busqueda)
     return render(request, "indumentaria.html", {'productos': productos, 'talles': talles, 'colores': colores, 'categorias': categorias})
