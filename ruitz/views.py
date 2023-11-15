1261b5a37e569fe90e0a47bd722c3a5f8b28ab57
AugustoGL <leonguchealezama@gmail.com>
2023-11-15 12:01:27 -0300

    filtro py

--- ruitz/views.py
+++ ruitz/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, redirect
-from .models import Producto
+from django.shortcuts import render
 from .models import *
 from .funcionalidades import *
+
 
 
 def Home(request):
@@ -19,9 +19,29 @@ def Indumentaria(request):
     talles = Tamaño.objects.all()
     colores = Color.objects.all()
     categorias = Categoria.objects.all()
-    productos = Producto.objects.all()
+    busqueda = request.GET.get('buscador')
+    productos, colores_seleccionados, categorias_seleccionadas, talles_seleccionados = filtrar_productos(request)
+    if busqueda:
+        productos = buscar(busqueda)
+    return render(request, 'indumentaria.html', {
+        'productos': productos,
+        'colores': colores,
+        'categorias': categorias,
+        'talles': talles,
+        'colores_seleccionados': colores_seleccionados,
+        'categorias_seleccionadas': categorias_seleccionadas,
+        'talles_seleccionados': talles_seleccionados,
+    })
 
+
+
+
+
+
+def Detalles(request, pk):
+    producto = Producto.objects.all(id=pk)
     busqueda = request.GET.get('buscador')
     if busqueda:
         productos = buscar(busqueda)
-    return render(request, "indumentaria.html", {'productos': productos, 'talles': talles, 'colores': colores, 'categorias': categorias})
+        return render(request, "indumentaria.html", {'productos': productos})
+    return render(request, "detalles.html", {'producto': producto})
