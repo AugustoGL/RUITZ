85ace4742f945b95f3347da69587cc6baa25308f
AugustoGL <aguchealezama@gmail.com>
2024-02-20 23:00:20 -0300

    dominio

--- ruitz/admin.py
+++ ruitz/admin.py
@@ -3,6 +3,11 @@ from .models import Categoria
 from .models import Color
 from .models import Tamaño
 from .models import Producto
+from django.contrib import admin
+from django.contrib.sites.models import Site
+
+admin.site.unregister(Site)  # Desregistrar la configuración de sitio predeterminada
+admin.site.site_url = 'https://tallerismo.com.ar'  # Configurar la URL del sitio correctamente
 
 admin.site.register(Categoria)
 admin.site.register(Color)
